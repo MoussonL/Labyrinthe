2fcc0b2ae2c97b4d7ebdaa274fb76c5bdb75d28b
Moerava <45635249+Moerava@users.noreply.github.com>
2018-12-13 16:32:23 +0100

    Update Affichage.c

--- Affichage.c
+++ Affichage.c
@@ -5,26 +5,6 @@
 void aff(Maze M) //val1 = M[i][j] | val2 = M[i][j-1] 
 {
 	int i,j,k;
-/*
-
-	for (k = 0; k < M.Col; k++)
-		{
-			if((M.Matrix[k][j]/2)%2 == 1)
-			{
-				printf("+---+"); //mur du bas
-			}
-			if((M.Matrix[i][j]/2)%2 == 0)
-			{
-				//if(M.Matrix[i][j]/%2 == 0)
-				printf("   "); //mur du bas
-			}
-			else _
-			{
-				printf("   ");
-			}
-		}
-
-*/
 
 	for ( i = 0; i < M.Col; i++) //Premier mur du haut
 	{
@@ -41,7 +21,14 @@ void aff(Maze M) //val1 = M[i][j] | val2 = M[i][j-1]
 		}
 		else
 		{
-			printf("   ");
+			if ((M.Matrix[0][i-1]/8)%2 == 0)//Si il n'y a pas déjà un mur en bas précedement, 
+				{                           //il faut marquer l'intersection de deux case
+					printf("+   ");
+				}
+				else
+				{
+				    printf("   ");
+			    }
 		}
 	}
 	printf("\n");
@@ -50,14 +37,14 @@ void aff(Maze M) //val1 = M[i][j] | val2 = M[i][j-1]
 	{
 		for (j = 0; j < M.Col; j++)
 		{
-			if(M.Matrix[i][j]%2 == 1)//mur de gauche
+			if(M.Matrix[i][j]%2 == 1)//Affiche le mur de gauche
 			{
 				if(M.Matrix[i][j-1]/4%2 == 0)
 				{
 					printf("|"); 
 				}
 			}
-			if((M.Matrix[i][j]/4)%2 == 1) //mur de droite
+			if((M.Matrix[i][j]/4)%2 == 1) //Affiche le mur de droite
 			{
 				if (M.Matrix[i][j]%2 == 1)
 				{
@@ -70,23 +57,21 @@ void aff(Maze M) //val1 = M[i][j] | val2 = M[i][j-1]
 					//printf("+---+");
 				} 
 			}
-			else					//Mur ni Ã  droite ni Ã  gauche
+			else							//Mur ni Ã  droite ni Ã  gauche
 			{	
 				if(M.Matrix[i][j]%2 == 0 && (M.Matrix[i][j]/4)%2 == 0)	
 					{	
 						printf("    "); // espace supplémentaire pour compenser l'intersection
-						//printf("+---+");
 					}
 				else
 				{
 					printf("   ");
-					//printf("+---+");
 				}
 				
 			}
 		}
 			printf("\n");
-		for (j = 0; j < M.Col; j++) //Mur du bas
+		for (j = 0; j < M.Col; j++) //Affiche mur du bas
 		{
 			if((M.Matrix[i][j]/2)%2 == 1)
 			{
