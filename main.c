5acdc70fb33d9431e82da7e24e305a9736a71965
MoussonL <45635207+MoussonL@users.noreply.github.com>
2018-12-15 19:19:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main()
 	int choice;
 	srand(time(NULL)); 
 	char* fic_save=(char*)calloc(50,sizeof(char));
-	printf("Bonjour, si vous souhaitez un lab a partir d'un fichier tapez 1, un lab aléatoire tapez 2 , un labyrinthe pseudo aléatoire tapez 3, un labyrinthe avec piles tapez 4\n");
+	printf("Bonjour, choisissez une option de génération du labyrinthe:\n1- d'après un fichier \n2- de façon totalement aléatoire\n3- de façon pseudo aléatoire\n4-avec des piles (toutes les cases accessibles depuis l'entrée)\n");
 	scanf("%d",&val);
 	Maze m;
 	Path path;
